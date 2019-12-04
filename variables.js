65b4228d97762dc951b55618e8a05bb6e9a0061d
Kali3D <xleto@hotmail.com>
2019-12-04 18:25:28 +0100

    variables.html

---
+++ variables.js
@@ -0,0 +1,24 @@
+var aNumber = 5.5;
+console.log("aNumber : ", aNumber);
+
+var aString = "une chaine de caractères";
+console.log("une chaine : ", aString);
+
+var aBoolean = false;
+console.log("aBoolean : ", false);
+
+var anArray = ["une chaine", 5, 10.23, true];
+console.log("anArray : ", anArray);
+
+var anObject = {
+	uneChaine: "test",
+	unEntier: 5,
+	unBoolean: false,
+	anArray: ["test", "retest", 5],
+	anotherObject: {
+		field1: "test",
+		field2: "retest"
+	}
+};
+
+console.log("anObject : ", anObject);
